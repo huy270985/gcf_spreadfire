0fb972e4a00ba39ad96decae6a9ef14426cc7ccd
Chau Hoang <kidphys@gmail.com>
2017-09-24 18:02:02 +0700

    expiredAt and active moved to account node

--- functions/index.js
+++ functions/index.js
@@ -6,74 +6,44 @@ const moment = require('moment');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref);
-  });
-});
+function getUpdateObj(profile) {
+    var d = moment(profile.startDate);
+    d.add(+profile.duration, 'months');
+    var expiredAt = d.toISOString()
+    var active = moment() <= d;
+    return {
+      'account/expiredAt': expiredAt,
+      'account/active': active
+   }
+}
 
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onWrite(event => {
-      // Grab the current value of what was written to the Realtime Database.
-      const original = event.data.val();
-      console.log('Uppercasing', event.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an 'uppercase' sibling in the Realtime Database returns a Promise.
-      return event.data.ref.parent.child('uppercase').set(uppercase);
-    });
-
-exports.triggerUpdateUserDuration = functions.database.ref('/users/{userId}/profile/duration')
+exports.triggerUpdateAccountOnProfileChanged = functions.database.ref('/users/{userId}/profile')
     .onUpdate(event => {
-      var deferred = Promise.defer();
-      event.data.ref.parent.once('value', function(data) {
-        console.log('Updating user expiration', event.params.userId, data.val());
-        var d = moment(data.startDate);
-        d.add(+data.duration, 'months');
-        data.expiredAt = d.toISOString()
-        data.active = moment() <= d;
-        var updateObj = {
-          'profile/expiredAt': data.expiredAt,
-          'account/active': data.active
-        }
-        event.data.ref.parent.parent.update(data).then(function() {
-          deferred.resolve();
-          console.log('Finished update', updateObj, event.data.ref.parent.parent.key);
-        })
-      })
-      return deferred.promise;
-    });
+      var profile = event.data.val();
+      console.log('Updating user expiration', event.params.userId, profile);
+      var updateObj = getUpdateObj(profile);
+      console.log('Updating on node', event.data.ref.parent.key);
+      return event.data.ref.parent.update(updateObj);
+  });
 
-exports.dailyUserExpirationCheck = functions.https.onRequest((req, res) => {
+
+exports.httpDailyUserExpirationCheck = functions.https.onRequest((req, res) => {
   var usersRef = admin.database().ref('/users').orderByChild('account/active').equalTo(true);
   usersRef.once('value', function(data) {
     var users = data.val();
     var updateObj = {};
     Object.keys(users).map(function(id) {
-      var d = moment(data.startDate);
-      d.add(+data.duration, 'months');
-      data.expiredAt = d.toISOString()
-      if(!users[id].profile.expiredAt) {
-        var d = moment(users[id].profile.startDate);
-        d.add(+users[id].profile.duration, 'months');
-        users[id].profile.expiredAt = d.toISOString()
-      }
-      var d = moment(users[id].profile.expiredAt);
-      updateObj[id + '/account/active'] = moment() <= d;
-      updateObj[id + '/profile/expiredAt'] = d.toISOString();
+      var profile = users[id].profile;
+      var account = users[id].account;
+      var userUpdateObj = getUpdateObj(profile);
+      console.log('Calculating update data for user:', users[id]);
+      console.log('Finished with updateObj', userUpdateObj);
+      updateObj[id + '/account/active'] = userUpdateObj['account/active'];
+      updateObj[id + '/account/expiredAt'] = userUpdateObj['account/expiredAt'];
     });
-    admin.database().ref('/users').update(updateObj).then(function() {
-      res.json(updateObj);
-    })
+    res.json(updateObj);
+    // admin.database().ref('/users').update(updateObj).then(function() {
+    //   res.json(updateObj);
+    // })
   })
 });
